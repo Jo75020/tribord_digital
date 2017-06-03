7ba77a8b84df54f42be58a83024ab8be8bf0dba4
Jordan Demmouche <jordand75020@hotmail.fr>
2017-06-03 17:01:46 +0200

    add flux instagram

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,11 @@ class PagesController < ApplicationController
 
   def agence
     @articles = Article.all
+    @instagram = open("https://api.instagram.com/v1/users/self/media/recent/?access_token=503524293.e52e6e5.dbad145197684c509fe9ec90ac2520cf")
+    @instagram.each do |i|
+     @result = JSON.parse(i)
+     @data = @result["data"]
+    end
   end
 
 end
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,5 @@
+ Instagram.configure do |config|
+  config.client_id     = "e52e6e5499c84bfdb9483d5a936290b0"
+  config.client_secret = "a3a0ba6b8c7e4c00a90503fe180a42c4"
+  config.access_token  = "503524293.e52e6e5.dbad145197684c509fe9ec90ac2520cf"
+end
