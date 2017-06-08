f1f81b6a84e0c7c0166b86a27f145a5e597e2d77
Jordan Demmouche <jordand75020@hotmail.fr>
2017-06-08 16:51:29 +0200

    main menu details

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('#burger-navbar').on('click', function(event) {
+    $('.dropdown').removeClass('dropdown');
        $('body').toggleClass('o-hidden');
       $('.mainMenu').toggleClass('mainMenu--active');
       if ($("#burger-navbar").hasClass("blacked-burger")) {
